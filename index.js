939c07038dc7f3e46395a49830e58adfdc6ea34a
Mendy Cohen <mendybeltcohen@gmail.com>
2019-11-04 22:01:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+let newID = Math.parseFloat("3.2");
+let var1 = 8;
+let var2 = 8;
+let multiply = var1 * var2;
+let randomNumber = Math.random() + 3;
